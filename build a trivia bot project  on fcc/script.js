6d5bca25fa8153a0a22cbef781feaefaed1bb405
file8ite <conor6658@gmail.com>
2025-09-29 01:49:32 +0100

    Build a trivia bo project  on fcc (My second commit practicing javascript on freecodecamp,  practicing variables.

---
+++ build a trivia bot project  on fcc/script.js
@@ -0,0 +1,33 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName;
+
+botName = 'Chijioke';
+
+let botLocation;
+
+botLocation = "The universe";
+
+let favoriteLanguage;
+
+favoriteLanguage = "English";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+favoriteLanguage = "python";
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage + " can build anything ";
+
+console.log(codingFact);
+
+codingFact = "Just learn " + favoriteLanguage;
+
+console.log(codingFact);
+
+codingFact = "I love " + favoriteLanguage;
+
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
